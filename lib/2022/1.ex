f090f01906844e741b3997a3ada041ab787e1399
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-12-06 18:09:42 +0100

    Solve first day part 2

--- lib/2022/1.ex
+++ lib/2022/1.ex
@@ -18,5 +18,20 @@ aoc 2022, 1 do
   end
 
   def p2(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(fn value ->
+      case value do
+        :error -> :error
+        {value, _} -> value
+      end
+    end)
+    |> Enum.chunk_by(&(&1 == :error))
+    |> Enum.filter(fn chunk -> chunk != [:error] end)
+    |> Enum.map(fn chunk -> Enum.sum(chunk) end)
+    |> Enum.sort(&>=/2)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
