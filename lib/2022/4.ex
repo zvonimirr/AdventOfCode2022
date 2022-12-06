b58191c4e1f147a305f9ef7b07a7038cec61b5a7
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-12-06 19:27:10 +0100

    Solve fourth day

--- lib/2022/4.ex
+++ lib/2022/4.ex
@@ -2,8 +2,40 @@ import AOC
 
 aoc 2022, 4 do
   def p1(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(fn [first, second] ->
+      [ff, fs] = String.split(first, "-")
+      [sf, ss] = String.split(second, "-")
+
+      frange = String.to_integer(ff)..String.to_integer(fs) |> Enum.to_list()
+      srange = String.to_integer(sf)..String.to_integer(ss) |> Enum.to_list()
+
+      {frange, srange}
+    end)
+    |> Enum.map(fn {flist, slist} ->
+      Enum.all?(flist, &(&1 in slist)) or Enum.all?(slist, &(&1 in flist))
+    end)
+    |> Enum.count(& &1)
   end
 
   def p2(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(fn [first, second] ->
+      [ff, fs] = String.split(first, "-")
+      [sf, ss] = String.split(second, "-")
+
+      frange = String.to_integer(ff)..String.to_integer(fs) |> Enum.to_list()
+      srange = String.to_integer(sf)..String.to_integer(ss) |> Enum.to_list()
+
+      {frange, srange}
+    end)
+    |> Enum.map(fn {flist, slist} ->
+      Enum.any?(flist, &(&1 in slist)) or Enum.any?(slist, &(&1 in flist))
+    end)
+    |> Enum.count(& &1)
   end
 end
