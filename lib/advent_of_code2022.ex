61e8fec5ded9d34cb29c185ea1ceade2139d41df
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-12-06 17:36:07 +0100

    Initial commit

---
+++ lib/advent_of_code2022.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2022 do
+  @moduledoc """
+  Documentation for `AdventOfCode2022`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2022.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code2022_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2022Test do
+  use ExUnit.Case
+  doctest AdventOfCode2022
+
+  test "greets the world" do
+    assert AdventOfCode2022.hello() == :world
+  end
+end
