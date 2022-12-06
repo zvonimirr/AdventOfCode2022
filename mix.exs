338fa39964c77674edc1f4c995a78bf9a9834ba3
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2022-12-06 17:40:59 +0100

    Fetch challenges

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule AdventOfCode2022.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:advent_of_code_utils, "~> 3.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
